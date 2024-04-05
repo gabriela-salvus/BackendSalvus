51bf18a1019dffde37e932ce1620f9c88d9d9185
gabriela-salvus <gabriela.teixeira@salvus.me>
2024-04-05 10:03:06 -0300

    modifiquei para que so fosse possivel cadastrar livros em maiusculo

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ server.listen(8080, () => {
 });
 
 server.post('/cadastrar',async (req,res) => {
+    if (req.body.titulo) {
+        req.body.titulo = req.body.titulo.toUpperCase();
+    }
+    
+    if (req.body.genero) {
+        req.body.genero = req.body.genero.toUpperCase();
+    }
+
+    if (req.body.status) {
+        req.body.status = req.body.status.toUpperCase();
+    }
+
     console.log(req.body);
 
     await Livros.create(req.body)
